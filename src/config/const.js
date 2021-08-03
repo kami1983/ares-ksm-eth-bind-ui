1e3ed4abee43ec1dd92846d0a80af91f3da7abae
kami1983 <kami@cancanyou.com>
2021-08-03 21:06:29 +0800

    Update online 2.

--- src/config/const.js
+++ src/config/const.js
@@ -2,6 +2,7 @@
 export const KUSAMA_SS58_FORMAT = 2
 export const ORIGIN_NAME = "Ares"
 export const BACKEND_VALID_URL = "http://45.77.243.246:8081/bind-eth"
-export const BACKEND_CONTRIBUTED_URL = "http://localhost:8080/get-extrinsic"
+// export const BACKEND_CONTRIBUTED_URL = "http://localhost:8080/get-extrinsic"
+export const BACKEND_CONTRIBUTED_URL = "http://45.77.243.246:8081/get-extrinsic"
 
 
